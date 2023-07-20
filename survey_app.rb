70cf79167cbdf3dc826d03f9475a18392be88892
Steve De Jongh <de.jongh.steve@gmail.com>
2023-07-20 13:14:17 -0700

    More set up

--- survey_app.rb
+++ survey_app.rb
@@ -4,3 +4,29 @@ require 'sinatra'
 require 'sinatra/contrib'
 require 'tilt/erubis'
 require 'bcrypt'
+
+require_relative 'database'
+
+configure do
+  enable :sessions
+  set :erb, :enable_html => true
+end
+
+configure(:development) do
+  require 'sinatra/reloader'
+  also_reload "database.rb"
+end
+
+helpers do
+  def signed_in?
+    true # Logic to do.
+  end
+end
+
+get '/' do
+  erb :home, layout: :layout
+end
+
+get '/signin' do
+  erb :sign_in, layout: :layout
+end
